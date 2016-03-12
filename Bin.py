096b3e8a4c33717fe3913bc2031adf4f0b7a34a3
Tyler Springer <mspringer@smu.edu>
2016-03-12 10:07:56 -0600

    Finished Single Node Failures where reservations can move anywhere

--- Bin.py
+++ Bin.py
@@ -1,4 +1,6 @@
 class UserError(Exception):
+    """ This class is used to throw customer exceptions """
+
     def __init__(self, value):
         self.value = value
 
@@ -41,7 +43,7 @@ class Bin:
         self.jobCount -= 1
 
     def empty_bin(self):
-        del self.jobs[:]
+        self.jobs = []
         self.jobCount = 0
         self.consumed = 0
         self.reserved = 0
--- generateSuccessBars.py
+++ generateSuccessBars.py
@@ -4,29 +4,28 @@ from scipy.stats import norm
 from math import sqrt
 import matplotlib.pyplot as plt
 
-if not os.path.isdir("./fragments_10000_pareto/"):
+path = "./fragments_10000_pareto/"
+
+if not os.path.isdir(path):
     raise ValueError("./fragments/ directory does not exist in the working directory")
 
-dirs = os.listdir("./fragments_10000_pareto/")
-dirs.sort()
-print dirs
+files = [f for f in os.listdir(path) if f.endswith(".csv")]
+files.sort()
 result_vector = np.zeros((40, 10000), dtype=float)
-result_vec = []
 
 base = 0.0
 reservation_percentages = []
 ticks = [0.0]
-for i in range(len(dirs)):
-    result_vec.append([])
+for i in range(len(files)):
     line_counter = 0
-    with open("./fragments_10000_pareto/" + dirs[i]) as fin:
+    with open(path + files[i]) as fin:
         for line in fin:
             sanitized_line = [token.rstrip("\n") for token in line.split(", ")]
             try:
                 result_vector[i, line_counter] = 1.0 - (float(sanitized_line[0])/float(sanitized_line[1]))
             except ValueError:
                 print sanitized_line
-                print "From: ", dirs[i]
+                print "From: ", files[i]
                 exit(1)
             line_counter += 1
     reservation_percentages.append(base)
@@ -34,30 +33,19 @@ for i in range(len(dirs)):
     if i % 2 == 1:
         ticks.append(base)
 
-mins = np.array(np.min(result_vector, axis=1))
-avgs = np.array(np.mean(result_vector, axis=1))
-maxs = np.array(np.max(result_vector, axis=1))
-std_devs = np.array(np.std(result_vector, axis=1))
+hundred_percent = np.zeros(40, dtype=float)
 
-z_critical = norm.ppf(q = 0.975)
-
-confidences = []
-sampleMeans = np.zeros(40, dtype=float)
-errs = np.zeros(40, dtype=float)
 for i in range(len(result_vector)):
-    if i == 3 or i == 4:
-        plt.hist(result_vector[i])
-        plt.show()
-    samples = np.zeros(50, dtype=float)
-    for j in range(50):
-        sample = np.random.choice(a=result_vector[i], size=1000)
-        sample_mean = sample.mean()
-        err = z_critical * (std_devs[i]/sqrt(1000))
-        samples[j] = err
-    confidences.append(samples.mean())
+    count = 0
+    for j in range(len(result_vector[i])):
+        if result_vector[i][j] == 1.0:
+            count += 1
+    hundred_percent[i] = float(count)/float(len(result_vector[i]))
 
-#transposed = [list(t) for t in zip(*confidences)]
+plt.bar(range(len(hundred_percent)), hundred_percent)
+plt.show()
 
+'''
 plt.figure(figsize=(16, 9))
 plt.plot(reservation_percentages, mins, linewidth=.5, linestyle='--', marker='x', color='r', label='Worst')
 plt.plot(reservation_percentages, avgs, linewidth=1.5, marker='o', color='b', label='Avg')
@@ -73,3 +61,4 @@ plt.xlabel('Reservation Percentage')
 plt.xticks(ticks)
 plt.legend(loc='lower right', shadow=True)
 plt.savefig("./fragments_10000_pareto/Results1000_10000_Pareto.png", dpi=300)
+'''
